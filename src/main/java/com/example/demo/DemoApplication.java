0b1d2c1ed0801d16e11a253222a1e8233a8aba56
ryoichi551124 <ryoichi551124@icloud.com>
2025-03-15 23:15:04 +0900

    DI Test3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,17 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.demo.service.BusinessLogic;
+import com.example.demo.example.Example;
 
 /**
  * SpringBoot起動クラス
  */
 @SpringBootApplication
 public class DemoApplication {
-	/**
-	 * SpringBoot起動
-	 * @param args
-	 */
+	/** 起動 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args)
 		.getBean(DemoApplication.class).execute();
@@ -23,19 +19,10 @@ public class DemoApplication {
 
 	/** DI */
 	@Autowired
-	@Qualifier("test")
-	private BusinessLogic business1; // TestLogicImplのインスタンス
+	private Example example;
 
-	/** DI */
-	@Autowired
-	@Qualifier("sample")
-	private BusinessLogic business2; // SampleLogicImplのインスタンス
-
-	/**
-	 * 実行
-	 */
+	/** 実行 */
 	private void execute() {
-		business1.doLogic();
-		business2.doLogic();
+		example.run();
 	}
 }
